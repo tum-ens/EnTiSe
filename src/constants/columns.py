ba20bcfcc10e107263a9658c20e433504ea94694
TUM-Doepfert <markus.doepfert@tum.de>
2024-12-16 17:28:58 +0100

    Implement constants for sharing strings across modules

---
+++ src/constants/columns.py
@@ -0,0 +1,6 @@
+class Columns:
+    """Column names used in timeseries data."""
+    DATETIME = "datetime"  # Timestamps
+    T_OUT = "temp_out"  # Outdoor temperature
+    LOAD = "load"  # Load values (e.g., electricity load)
+    DEMAND = "demand"  # Heating or cooling demand
---
+++ src/constants/objects.py
@@ -0,0 +1,7 @@
+class Objects:
+    """Keys used for identifying objects and their attributes."""
+    ID = "id"  # Unique identifier for an object
+    VERBOSE = "verbose"  # Verbose identifier
+    FILE = "file"  # File identifier
+    LOAD_BASE = "load_base"  # Base load identifier
+    WEATHER = "weather"  # Weather data identifier
