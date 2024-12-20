b7bc428d083849561011644727072bef123f5b06
TUM-Doepfert <markus.doepfert@tum.de>
2024-12-20 15:42:11 +0100

    New entries plus centralized dtypes

--- src/constants/general.py
+++ src/constants/general.py
@@ -1,10 +1,18 @@
 class Keys:
-    """General-purpose keys used in the tool."""
+    """General-purpose keys used in the tool.
+    Sorted alphabetically for easy reference.
+    """
+    COLUMNS = "columns"  # Key for specifying required columns
+    COLS_REQUIRED = "columns_required"  # Key for specifying required columns
+    COLS_OPTIONAL = "columns_optional"  # Key for specifying optional columns
+    DTYPE = "dtype"  # Key for specifying data type
     KEYS = "keys"  # Key for specifying required keys
+    KEYS_REQUIRED = "keys_required"  # Key for specifying required keys
+    KEYS_OPTIONAL = "keys_optional"  # Key for specifying optional keys
+    OPTIONAL = "optional"  # Key for specifying optional keys
+    REQUIRED = "required"  # Key for specifying required keys
     SUMMARY = "summary"  # Key for summary metrics
     TIMESERIES = "timeseries"  # Key for timeseries data
-    COLUMNS = "columns"  # Key for specifying required columns
-    DTYPE = "dtype"  # Key for specifying data type
 
 
 SEP = ":"  # Separator for any type of keys
