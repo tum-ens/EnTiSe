bdff0b0dd6b9892cdcde36715b92a3198e9d71e3
TUM-Doepfert <markus.doepfert@tum.de>
2025-02-10 11:29:48 +0100

    Add new constants

--- src/constants/columns.py
+++ src/constants/columns.py
@@ -9,12 +9,12 @@ class Columns:
     LOAD = "load"  # Load values (e.g., electricity load)
     OCCUPATION = "occupation"  # Occupancy data
     SOLAR_GHI = "solar_ghi"  # Global horizontal irradiance
-    T_OUT = "temp_out"  # Outdoor temperature
+    TEMP_OUT = "temp_out"  # Outdoor temperature
     DTYPES = {
         DATETIME: pd.Timestamp | np.datetime64 | str | int | float,
         DEMAND: float | int | np.number,
         LOAD: float | int | np.number,
         OCCUPATION: float,
         SOLAR_GHI: float | int | np.number,
-        T_OUT: float | int | np.number,
+        TEMP_OUT: float | int | np.number,
     }
