98ec1707f819d147fb892b432b5ced50958c4626
TUM-Doepfert <markus.doepfert@tum.de>
2025-05-27 12:48:29 +0200

    Add default configuration values for DHW calculations
    Introduce constants for temperature, density, specific heat, and seasonal variations used in domestic hot water (DHW) methods. These defaults will standardize key parameters and simplify future usage.

--- entise/methods/dhw/__init__.py
+++ entise/methods/dhw/__init__.py
@@ -4,34 +4,11 @@ DHW (Domestic Hot Water) methods for time series generation.
 This package contains methods for generating domestic hot water demand time series.
 """
 
-# Import the main facade class
-from entise.methods.dhw.probabilistic import ProbabilisticDHW
-
 # Import the Jordan & Vajen method
 from entise.methods.dhw.jordan_vajen import JordanVajen
 
-# Import utility functions
-from entise.methods.dhw.utils import (
-    get_activity_data, get_demand_data, get_cold_water_temperature, calculate_timeseries,
-    DEFAULT_TEMP_COLD, DEFAULT_TEMP_HOT, DEFAULT_SEASONAL_VARIATION, DEFAULT_SEASONAL_PEAK_DAY
-)
 
 __all__ = [
-    # Main facade class
-    'ProbabilisticDHW',
-
-    # Jordan & Vajen method
+    # Methods
     'JordanVajen',
-
-    # Utility functions
-    'get_activity_data',
-    'get_demand_data',
-    'get_cold_water_temperature',
-    'calculate_timeseries',
-
-    # Default values
-    'DEFAULT_TEMP_COLD',
-    'DEFAULT_TEMP_HOT',
-    'DEFAULT_SEASONAL_VARIATION',
-    'DEFAULT_SEASONAL_PEAK_DAY'
 ]
