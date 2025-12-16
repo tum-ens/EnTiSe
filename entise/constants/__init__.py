602f950c68115e3a22042a75433a7fdc9a2d8feb
TUM-Doepfert <markus.doepfert@tum.de>
2025-12-16 13:48:18 +0100

    Export individual constants and modules in entise/constants/__init__.py for tox: Tox did not work with the previous implicit exproach

--- entise/constants/__init__.py
+++ entise/constants/__init__.py
@@ -1,3 +1,18 @@
 """Centralized constants for consistent use across the tool."""
 
-__all__ = [key for key in dir() if not key.startswith("_")]
+from entise.constants.columns import Columns
+from entise.constants.constants import Constants, UnitConversion
+from entise.constants.general import SEP, Keys
+from entise.constants.objects import Objects
+from entise.constants.ts_types import VALID_TYPES, Types
+
+__all__ = [
+    "Columns",
+    "Constants",
+    "Keys",
+    "Objects",
+    "SEP",
+    "Types",
+    "UnitConversion",
+    "VALID_TYPES",
+]
